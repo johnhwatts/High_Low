5b3439ad800a0c5ad2683202205e8d3981db7609
John Watts <johnwatts@gmx.com>
2017-03-13 19:58:22 -0500

    Completed working game with notation

---
+++ high_low.php
@@ -0,0 +1,27 @@
+<?php
+
+$randomNumber = rand(1, 100); //Generate random number
+$playGame = true; //Initialize game with boolean so that we can end the game when user guesses right 
+
+fwrite(STDOUT, 'Guess the random number... '); //write to user
+
+$userNumber = fgets(STDIN); //get input from user
+
+//use a while loop with conditionals inside 
+while($playGame) {
+
+	if ($userNumber < $randomNumber) {
+	echo "HIGHER" . PHP_EOL;
+	fwrite(STDOUT, 'Guess the random number... ');
+	$userNumber = fgets(STDIN);
+
+	} else if ($userNumber > $randomNumber) {
+	echo "LOWER" . PHP_EOL;
+	fwrite(STDOUT, 'Guess the random number... ');
+	$userNumber = fgets(STDIN);
+
+	} else if ($randomNumber == $userNumber) {
+	echo "GOOD GUESS" . PHP_EOL;
+	$playGame = false;
+	}
+}
